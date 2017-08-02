6f2d391fbaa1963590f22b0d986482242c25e6b3
Muhammad Waseem <theprowaseem@gmail.com>
2017-08-02 14:08:42 +0500

    updating keyboard map

--- src/UrduKeyboard/KeyboardMap.js
+++ src/UrduKeyboard/KeyboardMap.js
@@ -1,6 +1,7 @@
 let keyboard = {};
+let hex;
 for(let i=32; i<127; i++){
-    let hex = Number(i).toString(16);
+    hex = Number(i).toString(16);
     hex = "00".substr(0, 6-hex.length) + hex;
     keyboard[i] = [String.fromCharCode(i), hex];
 }
